1c38da991ec2f905e6acc96850087e80f4c22209
juuhye <simjuuhye@gmail.com>
2024-07-22 21:00:03 +0900

    feat : board form, board view 댓글, 대댓글, 구조 수정 작업

--- common/js/ui.js
+++ common/js/ui.js
@@ -1,112 +1,140 @@
-document.addEventListener("DOMContentLoaded", function(){
-    scorllHeader();
-    alertEvt();
-  });
-  
-  function scorllHeader(){
-    let isScroll; //스크롤 상태 체크
-    let scrollPosition = 0; //스크롤 위치
-    const delta = 5; // 스크롤 감지 시작 위치
-    const header = document.querySelector('.headerBody') // scroll element
-    const headerH = header.offsetHeight;
-    
-    window.onscroll = function () {
-      isScroll = true;
-    };
-    
-    setInterval(function () {
-      if (isScroll) {
-        scrollCheck();
-        isScroll = false;
-      }
-    }, 250);
-    
-    function scrollCheck() {
-      var currentScrollTop = window.scrollY;
-      if (Math.abs(scrollPosition - currentScrollTop) <= delta) {
-        return;
-      }
-      if (currentScrollTop > scrollPosition && currentScrollTop > headerH) {
-        header.classList.remove('scroll');
-      } else if (currentScrollTop <= headerH) {
-        header.classList.remove('fixed');
-      } else {
-        if (currentScrollTop + window.innerHeight < document.body.offsetHeight) {
-          header.classList.add('scroll');
-          header.classList.add('fixed');
-        }
-      }
-      scrollPosition = currentScrollTop;
-    }
-  }
-  
-  function ancEvt(){
-    const links = document.querySelectorAll('.alertBtn'); 
-    
-    links.forEach(function(link) {
-      link.addEventListener("click", function(e) {
-        if (link.hash !== "") {
-          e.preventDefault();
-  
-          var hash = link.hash;
-          var targetElement = document.querySelector(hash);
-  
-          if (targetElement) {
-            var startPosition = window.pageYOffset;
-            var targetPosition = targetElement.offsetTop;
-            var distance = targetPosition - startPosition;
-            var duration = 800;
-            var startTime = null;
-  
-            function animate(currentTime) {
-              if (startTime === null) {
-                startTime = currentTime;
-              }
-  
-              var elapsedTime = currentTime - startTime;
-              var scrollPosition = easeInOutCubic(elapsedTime, startPosition, distance, duration);
-              window.scrollTo(0, scrollPosition);
-  
-              if (elapsedTime < duration) {
-                requestAnimationFrame(animate);
-              } else {
-                window.location.hash = hash;
-              }
-            }
-  
-            function easeInOutCubic(t, b, c, d) {
-              t /= d / 2;
-              if (t < 1) return c / 2 * t * t * t + b;
-              t -= 2;
-              return c / 2 * (t * t * t + 2) + b;
-            }
-  
-            requestAnimationFrame(animate);
-          }
-        }
-      });
-    });
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  handleScrollHeader();
+  setupAlertPopup();
+  setupAnchorScrolling();
+});
 
-  function alertEvt() {
-    let popupState = false;
-    const alertBtn = document.querySelector('.alertBtn'); 
-    const alertPopup = document.querySelector('.alertPopup');
-      
-    alertBtn.addEventListener('click', function(e){
-      alertPopup.classList.add('on');
-      e.stopPropagation()
-       return popupState = true; 
-    });
+
+function handleScrollHeader() {
+  let isScrolling; // 스크롤 상태 플래그
+  let lastScrollTop = 0; // 마지막 스크롤 위치
+  const scrollThreshold = 5; // 스크롤 감지 임계값
+  const header = document.querySelector('.headerBody'); // 헤더 요소
+  const headerHeight = header.offsetHeight; // 헤더 높이
   
-    
-    
-    document.body.addEventListener('click', function(e){
-      if(popupState == true){
-        if (!alertPopup.contains(e.target)){
-          alertPopup.classList.remove('on');
-          return popupState = false;
+  window.onscroll = function() {
+      isScrolling = true;
+  };
+  
+  setInterval(function() {
+      if (isScrolling) {
+          checkScrollPosition();
+          isScrolling = false;
+      }
+  }, 250);
+  
+  function checkScrollPosition() {
+      const currentScrollTop = window.scrollY;
+      
+      if (Math.abs(lastScrollTop - currentScrollTop) <= scrollThreshold) {
+          return;
+      }
+      
+      if (currentScrollTop > lastScrollTop && currentScrollTop > headerHeight) {
+          header.classList.remove('scroll');
+      } else if (currentScrollTop <= headerHeight) {
+          header.classList.remove('fixed');
+      } else {
+          if (currentScrollTop + window.innerHeight < document.body.offsetHeight) {
+              header.classList.add('scroll');
+              header.classList.add('fixed');
           }
-        }
+      }
+      
+      lastScrollTop = currentScrollTop;
+  }
+}
+
+function setupAnchorScrolling() {
+  const anchorLinks = document.querySelectorAll('.alertBtn'); 
+  
+  anchorLinks.forEach(link => {
+      link.addEventListener("click", function(event) {
+          if (link.hash !== "") {
+              event.preventDefault();
+              
+              const targetId = link.hash.substring(1);
+              const targetElement = document.getElementById(targetId);
+              
+              if (targetElement) {
+                  smoothScrollTo(targetElement);
+              }
+          }
       });
+  });
+
+  // anc 클릭시 해당 영역으로 scroll
+  function smoothScrollTo(targetElement) {
+      const startPosition = window.pageYOffset;
+      const targetPosition = targetElement.offsetTop;
+      const distance = targetPosition - startPosition;
+      const duration = 800;
+      let startTime = null;
+      
+      function animateScroll(currentTime) {
+          if (startTime === null) {
+              startTime = currentTime;
+          }
+          
+          const elapsedTime = currentTime - startTime;
+          const scrollPosition = easeInOutCubic(elapsedTime, startPosition, distance, duration);
+          window.scrollTo(0, scrollPosition);
+          
+          if (elapsedTime < duration) {
+              requestAnimationFrame(animateScroll);
+          } else {
+              window.location.hash = targetElement.id;
+          }
+      }
+      
+      function easeInOutCubic(time, start, change, duration) {
+          time /= duration / 2;
+          if (time < 1) return change / 2 * time * time * time + start;
+          time -= 2;
+          return change / 2 * (time * time * time + 2) + start;
+      }
+      
+      requestAnimationFrame(animateScroll);
+  }
+}
+
+// 알림 팝업 처리
+function setupAlertPopup() {
+  let isPopupVisible = false; 
+  const alertButton = document.querySelector('.alertBtn'); 
+  const alertPopup = document.querySelector('.alertPopup');
+
+  alertButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    if (!isPopupVisible) {
+      alertPopup.classList.add('on');
+      isPopupVisible = true;
+    }else {
+      alertPopup.classList.remove('on');
+      isPopupVisible = false;
     }
+  });
+
+  document.body.addEventListener('click', function(event) {
+    if (isPopupVisible && !alertPopup.contains(event.target) && !alertButton.contains(event.target)) {
+      alertPopup.classList.remove('on');
+      isPopupVisible = false;
+    }
+  });
+
+  alertPopup.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+}
+
+// 상단 헤더 공지사항 swiper
+var swiper = new Swiper(".noticeSwiper", {
+  direction: "vertical",
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  speed: 800,
+  loop: true,
+  touchRatio: 0
+}); 
